fc6cf4899d04d4ffd24de3bc6a6eeed31347251d
RuoBai <GLgele@outlook.com>
2023-08-15 22:00:21 +0800

    [Fix] AddExample

--- RazorPagesMovie/Data/SeedData.cs
+++ RazorPagesMovie/Data/SeedData.cs
@@ -49,40 +49,43 @@ public static class SeedData
                 logger.Error("Null RazorPagesMovieContext");
                 throw new ArgumentNullException("Null RazorPagesMovieContext");
             }
-        context.Movie.AddRange(
-        new Movie
-        {
-            Title = "When Harry Met Sally",
-            ReleaseDate = DateTime.Parse("1989-2-12"),
-            Genre = "Romantic Comedy",
-            Price = 7.99M
-        },
+            if (context.Movie.Count() == 1)
+            {
+                context.Movie.AddRange(
+            new Movie
+            {
+                Title = "When Harry Met Sally",
+                ReleaseDate = DateTime.Parse("1989-2-12"),
+                Genre = "Romantic Comedy",
+                Price = 7.99M
+            },
 
-        new Movie
-        {
-            Title = "Ghostbusters ",
-            ReleaseDate = DateTime.Parse("1984-3-13"),
-            Genre = "Comedy",
-            Price = 8.99M
-        },
+            new Movie
+            {
+                Title = "Ghostbusters ",
+                ReleaseDate = DateTime.Parse("1984-3-13"),
+                Genre = "Comedy",
+                Price = 8.99M
+            },
 
-        new Movie
-        {
-            Title = "Ghostbusters 2",
-            ReleaseDate = DateTime.Parse("1986-2-23"),
-            Genre = "Comedy",
-            Price = 9.99M
-        },
+            new Movie
+            {
+                Title = "Ghostbusters 2",
+                ReleaseDate = DateTime.Parse("1986-2-23"),
+                Genre = "Comedy",
+                Price = 9.99M
+            },
 
-        new Movie
-        {
-            Title = "Rio Bravo",
-            ReleaseDate = DateTime.Parse("1959-4-15"),
-            Genre = "Western",
-            Price = 3.99M
-        });
-        context.SaveChanges();
-        logger.Info("Example data inserted.");
+            new Movie
+            {
+                Title = "Rio Bravo",
+                ReleaseDate = DateTime.Parse("1959-4-15"),
+                Genre = "Western",
+                Price = 3.99M
+            });
+                context.SaveChanges();
+                logger.Info("Example data inserted.");
+            }
         }
     }
 }

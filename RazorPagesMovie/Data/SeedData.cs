b2e1d0cc2b85345a9a04b2a6d5255b899a751ac7
RuoBai <GLgele@outlook.com>
2023-08-20 20:40:00 +0800

    [New] Rating Column [Fix] Data Annotations

--- RazorPagesMovie/Data/SeedData.cs
+++ RazorPagesMovie/Data/SeedData.cs
@@ -31,7 +31,8 @@ public static class SeedData
                     Title = "Place Holder",
                     ReleaseDate = DateTime.Now,
                     Genre = "Placeholder",
-                    Price = 0.0M
+                    Price = 0.0M,
+                    Rating = "NA"
                 }
             );
             context.SaveChanges();
@@ -57,7 +58,8 @@ public static class SeedData
                 Title = "When Harry Met Sally",
                 ReleaseDate = DateTime.Parse("1989-2-12"),
                 Genre = "Romantic Comedy",
-                Price = 7.99M
+                Price = 7.99M,
+                Rating = "R"
             },
 
             new Movie
@@ -65,7 +67,8 @@ public static class SeedData
                 Title = "Ghostbusters ",
                 ReleaseDate = DateTime.Parse("1984-3-13"),
                 Genre = "Comedy",
-                Price = 8.99M
+                Price = 8.99M,
+                Rating = "G"
             },
 
             new Movie
@@ -73,7 +76,8 @@ public static class SeedData
                 Title = "Ghostbusters 2",
                 ReleaseDate = DateTime.Parse("1986-2-23"),
                 Genre = "Comedy",
-                Price = 9.99M
+                Price = 9.99M,
+                Rating = "G"
             },
 
             new Movie
@@ -81,7 +85,8 @@ public static class SeedData
                 Title = "Rio Bravo",
                 ReleaseDate = DateTime.Parse("1959-4-15"),
                 Genre = "Western",
-                Price = 3.99M
+                Price = 3.99M,
+                Rating = "NA"
             });
                 context.SaveChanges();
                 logger.Info("Example data inserted.");
